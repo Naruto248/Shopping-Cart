d1b06e7c3dc1f74228ebbc3c93ad9251afb399ab
Abhijeet Pandhe <abhijeetpandhe@gmail.com>
2018-06-27 21:05:22 +0530

    Link between controller and jsp is working

--- WebContent/WEB-INF/jsps/login.jsp
+++ WebContent/WEB-INF/jsps/login.jsp
@@ -1,6 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -8,9 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-<h1>This is index</h1>
-Hello <c:out value="${name }"></c:out>
-<br>
-<a href="<%= request.getContextPath()%>/Show-Login">Login</a>
+<h1>Login</h1>
 </body>
 </html>
--- src/com/shoppingcart/controller/CartController.java
+++ src/com/shoppingcart/controller/CartController.java
@@ -1,13 +1,22 @@
 package com.shoppingcart.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CartController {
 	
 	@RequestMapping("/")
-	public String showIndex(){
+	public String showIndex(Model model){
+		model.addAttribute("name", "Abhijeet");
 		return "index";
+	}
+	
+	@RequestMapping("/Show-Login")
+	public String showLogin(Model model){
+		return "login";
 	}
 }
